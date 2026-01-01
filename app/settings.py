6e36b2c3e8422c1fa5c1831e81313eccb0a2b247
saleem <ab588538@gmail.com>
2026-01-01 21:34:33 +0530

    add gunicorn to requirements

--- app/settings.py
+++ app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@9a8g9-wdgipbd32$@w-x2e@%p8w(w!lf*!v=e3c^4^$yg#&a)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
